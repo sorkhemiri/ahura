f25a5bf144fbd3e9daa04ac02153d5404082bcb2
sorkhemiri <sorkhemiri@gmail.com>
2020-02-19 12:33:06 +0330

    setup file modified

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'Ahura',
+  packages = ['Ahura'],
+  version = '0.1',
+  license='MIT',
+  description = 'A God Like Serializer For God Like Developers.',
+  author = 'Mahdi Sorkhemiri',
+  author_email = 'sorkhemiri@gmail.com',
+  url = 'https://github.com/sorkhemiri/ahura',
+  download_url = 'https://github.com/sorkhemiri/ahura/archive/0.1.tar.gz',
+  keywords = ['python', 'django', 'serializer', 'json', 'model', 'orm'],
+  install_requires=[
+          'django',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
