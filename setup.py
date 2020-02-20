bd03be7de3c0bcd2a16dbcd2f7f1b9c7d3161e7c
sorkhmiri <mahdisorkhmiri@gmail.com>
2020-02-20 20:02:04 +0330

    description fixed on pypi

--- setup.py
+++ setup.py
@@ -1,71 +1,12 @@
 from distutils.core import setup
 
-
-long_description  = "Ahura
-=======
-
-a God like Serializer for God like Developers.
-
-Getting Started
----------------
-
-just clone the project or use pip to install ahura and there should be no problem using it. ahura is available as django model seriliazer for now but it is goning to be a general python seriliazer very soon. feel free to contribute.
-
-Prerequisites
--------------
-
-Ahura is a Django Model Seriliazer for now so you must have django installed and use Django ORM. 
-
-Installing
-----------
-
-For using Ahura you just need to install it using pip.
-
-.. code-block:: console
-    
-    $pip install ahura
-
-or clone the project.then import the Seriliazer and use it like the example below.
-
-
-.. code-block:: python
-
-    >>> from ahura import Seriliazer
-    >>> from myapp.models import MyModel
-    >>> my_objects = MyModel.objects.all()
-    >>> s = Seriliazer(exclude=["password"], date_format="%Y-%m-%d")
-    >>> data = s.serialize(my_objects)
-
-
-Contributing
-------------
-
-Please read `CONTRIBUTING.md`_ for details on our code of conduct, and the process for submitting pull requests to us.
-
-Authors
--------
-* **Mahdi Sorkhemiri**  - *Initial work* - `Sorkhemiri`_
-* **Mohammad Rabetian**  - *Initial work* - `Rabetian`_
-
-See also the list of `contributors`_ who participated in this project.
-
-License
--------
-
-This project is licensed under the MIT License - see the `LICENSE.md`_ file for details
-
-
-.. _CONTRIBUTING.md: https://github.com/sorkhemiri/ahura/blob/master/CONTRIBUTING.md
-.. _Sorkhemiri: https://github.com/sorkhemiri
-.. _Rabetian: https://github.com/mohammadrabetian
-.. _contributors: https://github.com/sorkhemiri/ahura/graphs/contributors
-.. _LICENSE.md: https://github.com/sorkhemiri/ahura/blob/master/LICENSE.md
-"
+with open("README.rst", "r") as fl:
+    long_description = fl.read()
 
 setup(
   name = 'ahura',
   packages = ['ahura'],
-  version = '0.1.5',
+  version = '0.1.7',
   license='MIT',
   description = 'A God Like Serializer For God Like Developers.',
   long_description=long_description,
